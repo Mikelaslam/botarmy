b77f91fff187a3f5ef40b4a474558105861559e7
mikelaslam <kipkorir385@gmail.com>
2023-01-23 16:30:17 +0300

    Initial

---
+++ src/Components/BotCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const botTypeClasses = {
+  Assault: "icon military",
+  Defender: "icon shield",
+  Support: "icon plus circle",
+  Medic: "icon ambulance",
+  Witch: "icon magic",
+  Captain: "icon star",
+};
+
+
+function BotCard({bot, handleBot, handleDelete}) {
+ return (
+
+ )};
+
+ export default BotCard;
